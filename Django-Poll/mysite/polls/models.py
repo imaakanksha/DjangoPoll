d582e0bc450e20e33092f325e2beeda94fdd7fa2
Akanksha <akankshaf7@gmail.com>
2024-09-05 09:59:27 +0530

    New part is added in the programming.

--- Django-Poll/mysite/polls/models.py
+++ Django-Poll/mysite/polls/models.py
@@ -13,12 +13,12 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_text = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return f"{self.choice_text} ({self.question.question_text})"
 
 
 # Create your models here.
